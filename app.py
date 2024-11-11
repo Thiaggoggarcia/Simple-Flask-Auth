fd415d32b6c966182c2946e3074e537f9fa79200
thiaggoggarcia <thiaggoggarcia@gmail.com>
2024-11-10 23:52:00 -0300

    Conexão BD

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "minha_chave"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db = SQLAlchemy(app)
 
 @app.route("/hello-world",methods=['GET'])
 def index():
