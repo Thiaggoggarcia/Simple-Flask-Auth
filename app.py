2a96520ce0dc25b7747203f214e8fb306542d86f
thiaggoggarcia <thiaggoggarcia@gmail.com>
2024-11-10 23:51:35 -0300

    Conexão BD

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "minha_chave"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+
+@app.route("/hello-world",methods=['GET'])
+def index():
+    return "Hello World"
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5001)
