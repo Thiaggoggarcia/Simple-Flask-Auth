51b6c0ff1700ab51fcea6bcb6e5ad835ae44eb98
Tiago Garcia <thiaggoggarcia@gmail.com>
2024-12-23 17:31:41 -0300

    :sparkles: feat: Criando as rotas de Logout e cadastro de usuarios

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 from models.user import User
 from database import db
-from flask_login import LoginManager, login_user, current_user
+from flask_login import LoginManager, login_user, current_user, logout_user, login_required
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "minha_chave"
@@ -25,19 +25,35 @@ def login():
     
     if username and password:
         user = User.query.filter_by(username=username).first()
-        print("ffffffffffffffffffffffff",user)
         if user and user.password == password:
             login_user(user)
-            print(current_user.is_authenticated )
-            return jsonify({"Mensagem": "Autenticação Realizada com Sucesso!"})
+            print(current_user.is_authenticated)
+            return jsonify({"Mensagem": "Autenticacao Realizada com Sucesso!"})
+        else:
+            return jsonify({"Mensagem":"Usuario ou Senha incorreto!"}), 400
     else:
-        return jsonify({"Mensagem": "Credencial Inválida"}), 400
-
-@app.route("/login",methods=['GET'])
-def get_login():
-    data = request.json
+        return jsonify({"Mensagem": "Credencial Invalida"}), 400
     
-    return jsonify(data), 200  
+@app.route('/logout', methods=['GET'])
+@login_required
+def logout():
+    logout_user()
+    return jsonify({"Mensagem":"Logout Realizado com Sucesso!"})
+      
+@app.route("/user", methods=['POST'])
+#@login_required # Caso habilitado será permitido apenas o cadastro por pessoas já autênticadas no sistema      
+def create_user():
+    data = request.json
+    username = data.get('username')
+    password = data.get('password')
+    
+    if username and password:
+        user = User(username=username, password=password)
+        db.session.add(user)
+        db.session.commit()
+        return jsonify({"Mensagem":"Usuario Criado Com Sucesso!"})
+    
+    return jsonify({"Mensagem":"Erro ao Cadastrar Usuario!"}), 400 
     
 if __name__ == "__main__":
     app.run(debug=True, port=5001)
